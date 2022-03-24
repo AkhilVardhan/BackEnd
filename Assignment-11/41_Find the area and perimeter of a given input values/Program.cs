111aaa60ec29e1eb2a39125605d5da12848e236a
AkhilVardhan <39731842+AkhilVardhan@users.noreply.github.com>
2022-03-25 00:00:04 +0530

    Add files via upload

---
+++ Assignment-11/41_Find the area and perimeter of a given input values/Program.cs
@@ -0,0 +1,54 @@
+﻿/*41) WAP to find the Area, permiter for the below shapes
+    input : TypeOfShape ( you should accept either C or R or S)
+             Length/Radius , width
+    Note: if user chooses then system should take input for  width 
+    Output : The Area for the Circle is : 
+	     The Perimeter for Circle is :
+             The Area for the rectangle is :
+             The Perimeter for the Rectangle is :
+	     The Area for the the Square is :
+             The Perimeter for the Square is :
+*/
+using System;
+
+namespace _41_Find_the_area_and_perimeter_of_a_given_input_values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter Radius: ");
+            double Radious = Convert.ToDouble(Console.ReadLine());
+            double Area = Math.PI * Radious * Radious;
+            double r, per_cir;
+            double PI = 3.14;
+            Console.WriteLine("Enter the radius of the circle : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            per_cir = 2 * PI * r;
+            Console.WriteLine("Area of circle: " + Area);
+            Console.WriteLine("Perimeter of Circle : {0}", per_cir);
+            Console.ReadLine();
+            int l, w, area, perimeter;
+            Console.Write("Length : ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Width : ");
+            w = Convert.ToInt32(Console.ReadLine());
+            area = l * w;
+            perimeter = 2 * (l + w);
+            Console.WriteLine("Area of Rectangle : " + area);
+            Console.WriteLine("Perimeter of Rectangle : " + perimeter);
+            Console.ReadKey();
+            decimal side;
+            Console.WriteLine("Enter the Length of Side: ");
+            side = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("Enter the value of side : ");
+            //perimeter = (int)Convert.ToDecimal(Console.ReadLine());
+            area = (int)(side * side);
+            perimeter = (int)(4 * side);
+            Console.WriteLine("Area of Square: " + area);
+            Console.Write("Perimeter of Square is : " + perimeter);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment-11/Compound Interest Program/Program.cs
@@ -0,0 +1,44 @@
+﻿/*40) WAP to calculate yearly compound interest for the below inputs
+    Principal Amount , Loan Start dATE,  Rate of Interest per year
+*/
+using System;
+using System.Globalization;
+
+namespace Compound_Interest_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            decimal compoundintrest, amount, principal, rate;
+            int n, time;
+
+
+            Console.Write("Enter the principal amount = ");
+            principal = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("rate Of Intrest= ");
+            rate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter compound frequency / year = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the Loan start Date:");
+            DateTime dtLoanStartDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            int NumberOfYears = ((DateTime.Today - dtLoanStartDate).Days) / 365;
+            decimal interestAmount = 0, TotalAmountPower;
+            decimal originalPricipalAmount = principal;
+
+            for (int tempyear = 1; tempyear <= NumberOfYears; tempyear++)
+            {
+                interestAmount = principal * (rate / 100.0M);
+                principal = principal + interestAmount;
+
+            }
+
+            TotalAmountPower = originalPricipalAmount * (decimal)Math.Pow((double)(1 + (rate / 100.0M)), NumberOfYears);
+
+            // display result upto 2 decimal places
+            Console.WriteLine("The toal amount to be paid as on {1}'s date is {0}", Math.Round(principal, 2), DateTime.Today.ToShortDateString());
+            Console.WriteLine("The total Amount to be paid calculated using the fpower function as on {1}'s date is {0:N2}", TotalAmountPower, DateTime.Today.ToShortDateString());
+        }
+    }
+}
